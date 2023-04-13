896d604a06e256922f4d60a39747e87cd4abf8db
Brayan Sepúlveda <brayansepulveda@saimyr.com.co>
2023-04-13 11:50:40 -0500

    Se corrige bug que no redireccionaba al login cuando se inghrtesaba por la ruta abse de la aplicacion y se configura el metodo login del servicio auth para que retorne una promesa de un string

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: SidenavComponent,
+    canActivate: [AuthGuard],
     canActivateChild: [AuthGuard],
     children: [
       {
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse} from "@angular/common/http";
-import {EMPTY, of} from "rxjs";
 import {SignUpUser} from 'src/app/models/auth/sign-up-user';
 import {LoginUser} from 'src/app/models/auth/login-user';
 import {environment} from 'src/environments/environment';
-import {User} from "../../models/user/user";
 import {map} from "rxjs/operators";
 
 @Injectable({
@@ -20,16 +18,14 @@ export class AuthService {
     return this.httpClient.post<any>(methodURL, signUpUser).toPromise();
   }
 
-  public login(loginUser: LoginUser): Promise<{ user: User | undefined; token: string }> {
+  public login(loginUser: LoginUser): Promise<string> {
     const methodURL = `${environment.baseUrl}/login`;
     return this.httpClient.post<any>(methodURL, loginUser, {
       observe: 'response'
     }).pipe(map((response: HttpResponse<any>) => {
       const headers = response.headers;
       const bearerToken = headers.get('Authorization')!;
-      const token = bearerToken?.replace('Bearer ','');
-
-      return {user: undefined, token: token};
+      return bearerToken?.replace('Bearer ', '');
     })).toPromise();
   }
 }
